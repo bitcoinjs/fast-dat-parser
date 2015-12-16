2099f282632098b313a6525487dde04e62ece9aa
Daniel Cousens <github@dcousens.com>
2015-12-17 01:49:28 +1100

    bestchain: don't bother with pointers, just copy

--- bestchain.cpp
+++ bestchain.cpp
@@ -15,20 +15,21 @@ struct Block {
 	hash_t prevBlockHash;
 	uint32_t bits;
 
+	Block () {}
 	Block (const hash_t& hash, const hash_t& prevBlockHash, const uint32_t bits) : hash(hash), prevBlockHash(prevBlockHash), bits(bits) {}
 };
 
 struct Chain {
-	Block* block;
+	Block block;
 	Chain* previous;
 	size_t work = 0;
 
 	Chain () {}
-	Chain (Block* block, Chain* previous) : block(block), previous(previous) {}
+	Chain (Block block, Chain* previous) : block(block), previous(previous) {}
 
 	size_t determineAggregateWork () {
 		if (this->work != 0) return this->work;
-		this->work = this->block->bits;
+		this->work = this->block.bits;
 
 		if (this->previous != nullptr) {
 			this->work += this->previous->determineAggregateWork();
@@ -49,29 +50,29 @@ struct Chain {
 	}
 };
 
-auto& findChains(std::map<hash_t, Chain>& chains, const std::map<hash_t, Block*>& hashMap, Block* root) {
-	const auto blockChainIter = chains.find(root->hash);
+auto& findChains(std::map<hash_t, Chain>& chains, const std::map<hash_t, Block>& hashMap, const Block& root) {
+	const auto blockChainIter = chains.find(root.hash);
 
 	// already built this?
 	if (blockChainIter != chains.end()) return blockChainIter->second;
 
 	// not yet built, what about the previous block?
-	const auto prevBlockIter = hashMap.find(root->prevBlockHash);
+	const auto prevBlockIter = hashMap.find(root.prevBlockHash);
 
 	// if prevBlock is unknown, it must be a genesis block
 	if (prevBlockIter == hashMap.end()) {
-		chains[root->hash] = Chain(root, nullptr);
+		chains[root.hash] = Chain(root, nullptr);
 
-		return chains[root->hash];
+		return chains[root.hash];
 	}
 
 	// otherwise, recurse down to the genesis block, finding the chain on the way back
 	const auto prevBlock = prevBlockIter->second;
 	auto& prevBlockChain = findChains(chains, hashMap, prevBlock);
 
-	chains[root->hash] = Chain(root, &prevBlockChain);
+	chains[root.hash] = Chain(root, &prevBlockChain);
 
-	return chains[root->hash];
+	return chains[root.hash];
 }
 
 auto findChainTips(std::map<hash_t, Chain>& chains) {
@@ -81,13 +82,13 @@ auto findChainTips(std::map<hash_t, Chain>& chains) {
 		auto&& chain = chainIter.second;
 		if (chain.previous == nullptr) continue;
 
-		children[chain.previous->block->hash] = true;
+		children[chain.previous->block.hash] = true;
 	}
 
 	std::vector<Chain> tips;
 	for (auto& chainIter : chains) {
 		auto&& chain = chainIter.second;
-		if (children.find(chain.block->hash) != children.end()) continue;
+		if (children.find(chain.block.hash) != children.end()) continue;
 
 		tips.push_back(chain);
 	}
@@ -110,7 +111,7 @@ auto findBest(std::vector<Chain> chains) {
 
 	std::vector<Block> blockchain;
 	bestChain.forEach([&](const Chain& chain) {
-		blockchain.push_back(*(chain.block));
+		blockchain.push_back(chain.block);
 	});
 
 	return blockchain;
@@ -137,15 +138,15 @@ int main () {
 	} while (true);
 
 	// build a hash map for easy referencing
-	std::map<hash_t, Block*> hashMap;
-	for (size_t i = 0; i < blocks.size(); ++i) {
-		hashMap[blocks[i].hash] = &blocks[i];
+	std::map<hash_t, Block> hashMap;
+	for (auto& block : blocks) {
+		hashMap[block.hash] = block;
 	}
 
 	// find all possible chains
 	std::map<hash_t, Chain> chains;
 	for (auto& block : blocks) {
-		findChains(chains, hashMap, &block);
+		findChains(chains, hashMap, block);
 	}
 
 	const auto chainTips = findChainTips(chains);
