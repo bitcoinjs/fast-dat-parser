10b3f066dbf24e02f2bc6f5e3f160592d70b6b9c
Daniel Cousens <github@dcousens.com>
2016-01-22 13:28:22 +1100

    add binsort

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 W=-pedantic -std=c++14 -W -Wall -Wconversion -Wextra -Wfatal-errors -Wwrite-strings -Wno-unused-variable -Wno-unused-parameter
 
-all: bestchain parser
+all: binsort bestchain parser
+
+binsort: binsort.cpp
+	g++ $W -fopenmp -O3 $< -Iinclude -I. -std=c++14 -o $@
 
 bestchain: bestchain.cpp include/hash.hpp include/hvectors.hpp
 	g++ $W -O3 $< -Iinclude -I. libconsensus/*.cpp -std=c++14 -o $@
---
+++ binsort.cpp
@@ -0,0 +1,45 @@
+#include <parallel/algorithm>
+
+#include <cassert>
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+
+int main (int argc, char** argv) {
+	assert(argc > 2 && argc < 4);
+
+	size_t compareBytes = 0;
+	assert(sscanf(argv[1], "%lu", &compareBytes) == 1);
+
+	size_t dataBytes = 0;
+	if (argc == 3) {
+		assert(sscanf(argv[2], "%lu", &dataBytes) == 1);
+	}
+
+	const auto totalBytes = compareBytes + dataBytes;
+	std::vector<uint8_t*> vector;
+
+	while (true) {
+		const auto slice = new uint8_t[totalBytes];
+		const auto read = fread(slice, totalBytes, 1, stdin);
+
+		// EOF?
+		if (read == 0) break;
+
+		vector.emplace_back(slice);
+	}
+
+	__gnu_parallel::sort(
+		vector.begin(), vector.end(),
+		[compareBytes](const auto& a, const auto& b) {
+			return memcmp(a, b, compareBytes) < 0;
+		}
+	);
+
+	for (auto&& slice : vector) {
+		fwrite(slice, totalBytes, 1, stdout);
+	}
+
+	return 0;
+}
