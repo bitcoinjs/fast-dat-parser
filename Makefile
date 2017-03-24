794f58cd4eeef974508ed66ffb663f12cd1df2e8
Daniel Cousens <github@dcousens.com>
2017-03-24 14:23:13 +1100

    use OpenSSL sha256 over inline

--- Makefile
+++ Makefile
@@ -1,13 +1,16 @@
 CXX=g++
 CFLAGS=-pedantic -std=c++1z -W -Wall -Wcast-qual -Wconversion -Werror -Wextra -Wwrite-strings
+OFLAGS=-O3
+LFLAGS=-lcrypto
+IFLAGS=-Iinclude -I.
 
 all: bestchain parser
 
 bestchain: bestchain.cpp include/hash.hpp include/hvectors.hpp include/slice.hpp
-	$(CXX) $(CFLAGS) -O3 $< -Iinclude -I. libconsensus/*.cpp -o $@
+	$(CXX) $(CFLAGS) $(LFLAGS) $(OFLAGS) $(IFLAGS) $< -o $@
 
 parser: parser.cpp include/block.hpp include/hash.hpp include/hvectors.hpp include/threadpool.hpp include/transforms.hpp transforms/*.hpp include/slice.hpp
-	$(CXX) $(CFLAGS) -pthread -O3 $< -Iinclude -I. -lleveldb libconsensus/*.cpp -o $@
+	$(CXX) $(CFLAGS) $(LFLAGS) $(OFLAGS) $(IFLAGS) -lleveldb -pthread $< -o $@
 
 include/slice.hpp:
 	curl https://raw.githubusercontent.com/dcousens/ranger/master/slice.hpp > include/slice.hpp
--- include/hash.hpp
+++ include/hash.hpp
@@ -1,46 +1,18 @@
 #pragma once
 
 #include <array>
-#include "libconsensus/ripemd160.h"
-#include "libconsensus/sha256.h"
-
-void hash160 (uint8_t* dest, const uint8_t* src, size_t n) {
-	CSHA256 hash;
-	hash.Write(src, n);
-	hash.Finalize(dest);
-	CRIPEMD160 hashR;
-	hashR.Write(dest, 32);
-	hashR.Finalize(dest);
-}
-
-void hash256 (uint8_t* dest, const uint8_t* src, size_t n) {
-	CSHA256 hash;
-	hash.Write(src, n);
-	hash.Finalize(dest);
-	hash.Reset();
-	hash.Write(dest, 32);
-	hash.Finalize(dest);
-}
-
-void ripemd160 (uint8_t* dest, const uint8_t* src, size_t n) {
-	CRIPEMD160 hash;
-	hash.Write(src, n);
-	hash.Finalize(dest);
-}
-
-void sha256 (uint8_t* dest, const uint8_t* src, size_t n) {
-	CSHA256 hash;
-	hash.Write(src, n);
-	hash.Finalize(dest);
-}
-
-// Convenience wrappers
 #include "slice.hpp"
 
-typedef std::array<uint8_t, 20> uint160_t;
 typedef std::array<uint8_t, 32> uint256_t;
 
-void hash160 (uint8_t* dest, const Slice src) { hash160(dest, src.begin(), src.length()); }
-void hash256 (uint8_t* dest, const Slice src) { hash256(dest, src.begin(), src.length()); }
-void ripemd160 (uint8_t* dest, const Slice src) { ripemd160(dest, src.begin(), src.length()); }
-void sha256 (uint8_t* dest, const Slice src) { sha256(dest, src.begin(), src.length()); }
+void sha256 (uint8_t* dest, const Slice src) {
+	SHA256_CTX context;
+	SHA256_Init(&context);
+	SHA256_Update(&context, src.begin(), src.length());
+	SHA256_Final(dest, &context);
+}
+
+void sha256d (uint8_t* dest, const Slice src) {
+	sha256(dest, src, n);
+	sha256(dest, dest, 32);
+}
